0dd9a4c49d0170b978df70a82c0e14d130b2f467
Steven Lu <steven.lu@jibo.com>
2017-10-18 16:43:24 -0400

    tweaks to history

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -11,8 +11,8 @@
 #
 
 HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
-HISTSIZE=10000                   # The maximum number of events to save in the internal history.
-SAVEHIST=10000                   # The maximum number of events to save in the history file.
+HISTSIZE=50000                   # The maximum number of events to save in the internal history.
+SAVEHIST=1000000                   # The maximum number of events to save in the history file.
 
 #
 # Options
@@ -20,8 +20,8 @@ SAVEHIST=10000                   # The maximum number of events to save in the h
 
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY             # Share history between all sessions.
+setopt INC_APPEND_HISTORY_TIME   # Write to the history file when command finishes.
+# setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
